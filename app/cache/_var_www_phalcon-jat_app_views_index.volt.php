ddd370187efe2eac3d96b0c91e265ddcd33afc32
Nils Martinussen <nilsma231@gmail.com>
2015-05-06 17:08:20 +0200

    added google analytics

--- app/cache/_var_www_phalcon-jat_app_views_index.volt.php
+++ app/cache/_var_www_phalcon-jat_app_views_index.volt.php
@@ -6,7 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php echo $this->assets->outputCss(); ?>
     <?php echo $this->assets->outputJs(); ?>
-	<title>Job Application Tracker</title>
+	<script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-40419390-3', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+    <title>Job Application Tracker</title>
 	</head>
 	<?php
 	if($this->router->getControllerName() == "") {
